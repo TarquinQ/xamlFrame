0512ef98d3facb7815ec21aa5d148a2ecca75e8f
Milton Yates <milton.yates@csiro.au>
2018-07-13 17:55:53 +0930

    More Machine Info, and class for WindowUpdate Client Status added

--- xamlFrame/Lib/BasicMachineInfo.cs
+++ xamlFrame/Lib/BasicMachineInfo.cs
@@ -5,46 +5,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
+using System.IO;
+using Microsoft.Management.Infrastructure;
+using Microsoft.Win32;
 
 namespace xamlFrame.Lib
 {
     class BasicMachineInfo
     {
-        public string MachineName { get; }
         public string CurrUserNameFull { get; }
         public string CurrUserName { get; }
         public string CurrUserDomain { get; }
+        public string MachineName { get; }
         public string Manufacturer { get; }
         public string Model { get; }
         public string MachineSerial { get; }
+        public long RAMTotalB { get; }
+        public long RAMTotalGB { get; }
+        public string OSName { get; }
         public string OSVersion { get; }
         public string OSRevision { get; }
-        public int RAMTotalGB { get; }
+        public string OSArchitecture { get; }
+        public DateTime LastBootUpTime { get; }
+        public string SystemDriveLetter { get; }
+        public long SystemDriveSpaceTotal { get; }
+        public long SystemDriveSpaceUsed { get; }
+        public long SystemDriveSpaceFree { get; }
+        public long SystemDriveSpaceTotalGB { get; }
+        public long SystemDriveSpaceUsedGB { get; }
+        public long SystemDriveSpaceFreeGB { get; }
+        public int SystemDriveSpaceFreePercent { get; }
         //public ? WindowsExperience Rating -- Get-WmiObject -class Win32_WinSAT
         // * Primary IPv4 (w/ Default Route)
         // * Primary IPv6 (w/ Default Route)
+
         private readonly GetLocalCIMData CIMDataProvider;
 
         public BasicMachineInfo()
         {
             CIMDataProvider = new GetLocalCIMData();
+            string cimClassName;
+            CimInstance result;
 
             MachineName = Environment.MachineName;
             CurrUserNameFull = Environment.UserName; // domain\username
             CurrUserDomain = Environment.UserDomainName;
             CurrUserName = Environment.UserName.Remove(0, (CurrUserDomain.Length + 1));
 
-            string cimClassName = "Win32_ComputerSystem";
-            CIMDataProvider.GetEnumeratedInstances(className: cimClassName, firstInstance: out var result);
-
+            cimClassName = "Win32_ComputerSystem";
+            CIMDataProvider.GetEnumeratedInstances(className: cimClassName, firstInstance: out result);
             Manufacturer = result.CimInstanceProperties["Manufacturer"].Value.ToString();
             Model = result.CimInstanceProperties["Model"].Value.ToString();
-            int RAMTotalKB = result.CimInstanceProperties["TotalPhysicalMemory"].Value<int>();
-            RAMTotalGB = RAMTotalKB / 1024 / 1024;
 
             cimClassName = "Win32_Bios";
             CIMDataProvider.GetEnumeratedInstances(className: cimClassName, firstInstance: out result);
             MachineSerial = result.CimInstanceProperties["SerialNumber"].Value.ToString();
+
+            cimClassName = "Win32_PhysicalMemory";
+            foreach (CimInstance physicalMemory in CIMDataProvider.GetEnumeratedInstances(className: cimClassName)) {
+                RAMTotalB += physicalMemory.CimInstanceProperties["Capacity"].Value<long>();
+            }
+            RAMTotalGB = RAMTotalB / 1024 / 1024 / 1024;
+
+            //Caption, Version, BuildNumber, OSArchitecture
+            string releaseId = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "").ToString();
+            cimClassName = "Win32_OperatingSystem";
+            CIMDataProvider.GetEnumeratedInstances(className: cimClassName, firstInstance: out result);
+            OSArchitecture = result.CimInstanceProperties["OSArchitecture"].Value.ToString();
+            OSName = result.CimInstanceProperties["Caption"].Value.ToString() + " (" + OSArchitecture + ")";
+            OSVersion = result.CimInstanceProperties["Version"].Value.ToString();
+            if (String.IsNullOrWhiteSpace(releaseId))  // Is Not Win10...
+            {
+                OSRevision = result.CimInstanceProperties["BuildNumber"].Value.ToString();
+            }
+            else  // Is Win10
+            {
+                OSRevision = releaseId + " (" + result.CimInstanceProperties["BuildNumber"].Value.ToString() + ")";
+            }
+            LastBootUpTime = ManagementDateTimeConverter.ToDateTime(result.CimInstanceProperties["LastBootUpTime"].Value.ToString());
+
+
+            SystemDriveLetter = Path.GetPathRoot(Environment.SystemDirectory).Substring(0, 1);
+            DriveInfo systemDrive = new DriveInfo(SystemDriveLetter);
+            SystemDriveSpaceTotal = systemDrive.TotalSize;
+            SystemDriveSpaceFree = systemDrive.TotalFreeSpace;
+            SystemDriveSpaceUsed = SystemDriveSpaceTotal - SystemDriveSpaceFree;
+            SystemDriveSpaceTotalGB = SystemDriveSpaceTotal / 1024 / 1024 / 1024;
+            SystemDriveSpaceFreeGB = SystemDriveSpaceFree / 1024 / 1024 / 1024;
+            SystemDriveSpaceUsedGB = SystemDriveSpaceTotalGB - SystemDriveSpaceFreeGB;
+            SystemDriveSpaceFreePercent = unchecked((int)((100 * SystemDriveSpaceFree) / SystemDriveSpaceTotal));
         }
     }
 }
---
+++ xamlFrame/Lib/WindowsUpdatesClientStatus.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WUApiLib;
+
+namespace xamlFrame.Lib
+{
+    class InstalledUpdate
+    {
+        public InstalledUpdate(string Title, DateTime InstalledDate)
+        {
+            this.Title = Title;
+            this.InstalledDate = InstalledDate;
+        }
+
+        public string Title { get; set; }
+        public DateTime InstalledDate { get; set; }
+    }
+
+    class WindowsUpdatesClientStatus
+    {
+        public WindowsUpdatesClientStatus(bool enabled,
+            bool rebootRequired,
+            List<InstalledUpdate> installedUpdates,
+            DateTime? lastSearchSuccess,
+            DateTime? lastInstallationSuccess,
+            int countOutstanding,
+            DateTime oldestOutstanding)
+        {
+            this.Enabled = enabled;
+            this.RebootRequired = rebootRequired;
+            this.InstalledUpdates = installedUpdates;
+            this.LastSearchSuccess = lastSearchSuccess;
+            this.LastInstallationSuccess = lastInstallationSuccess;
+            this.CountOutstanding = countOutstanding;
+            this.OldestOutstanding = oldestOutstanding;
+        }
+
+        public bool Enabled { get; }
+        public bool RebootRequired { get; }
+        public List<InstalledUpdate> InstalledUpdates { get; }
+        public DateTime? LastSearchSuccess { get; }
+        public DateTime? LastInstallationSuccess { get; }
+        public int CountOutstanding { get; }
+        public DateTime OldestOutstanding { get; }
+    }
+
+    class WindowsUpdatesClientProxy
+    {
+        public WindowsUpdatesClientStatus GetInfoSummary(bool online = false)
+        {
+            List<InstalledUpdate> installedUpdates = GetInstalledUpdates();
+            Tuple<DateTime?, DateTime?> lastSearchAndInstall = GetDatesLastSearchAndInstall();
+            Tuple<int, DateTime, List<IUpdate>> outstandingUpdates = GetOutstandingUpdates(online: online);
+            return new WindowsUpdatesClientStatus(
+                enabled: IsWindowsUpdateEnabled(),
+                rebootRequired: IsRebootPending(),
+                installedUpdates: installedUpdates,
+                lastSearchSuccess: lastSearchAndInstall.Item1,
+                lastInstallationSuccess: lastSearchAndInstall.Item2,
+                countOutstanding: outstandingUpdates.Item1,
+                oldestOutstanding: outstandingUpdates.Item2
+            );
+        }
+
+        public Boolean IsWindowsUpdateEnabled()
+        {
+            var updates = new AutomaticUpdates();
+            return updates.ServiceEnabled;
+        }
+
+        public Boolean IsRebootPending()
+        {
+            // Note: This may need to be rewritten to use IUpdate2 for an IUpdateSearcher result of isInstalled=1
+            // See:  https://docs.microsoft.com/en-us/previous-versions/windows/desktop/aa386100%28v%3dvs.85%29
+            bool rebootRequired = false;
+
+            string regKeyReboot = @"SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired";
+            RegistryView regOSNativeView = RegistryView.Registry64;
+            if (! Environment.Is64BitOperatingSystem)
+            {
+                regOSNativeView = RegistryView.Registry32;
+            }
+            RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, regOSNativeView);
+            using (RegistryKey key = baseKey.OpenSubKey(regKeyReboot))
+            {
+                if (key is null)
+                {
+                    return false;
+                }
+
+                foreach (string valueName in key.GetValueNames())
+                {
+                    string value = Convert.ToString(key.GetValue(valueName));
+                    if (value.Equals("1"))
+                    {
+                        rebootRequired = true;
+                    }
+                }
+            }
+            return rebootRequired;
+        }
+
+        private IUpdateSearcher GetNewSearcher(bool online = false)
+        {
+            UpdateSession updateSession = new UpdateSession();
+            IUpdateSearcher updateSearcher = updateSession.CreateUpdateSearcher();
+            updateSearcher.Online = online;
+            return updateSearcher;
+        }
+
+        public List<InstalledUpdate> GetInstalledUpdates()
+        {
+            IUpdateSearcher updateSearcher = GetNewSearcher(online: false);
+            int count = updateSearcher.GetTotalHistoryCount();
+            IUpdateHistoryEntryCollection history = updateSearcher.QueryHistory(0, count);
+            List<InstalledUpdate> installedUpdates = new List<InstalledUpdate>();
+            for (int i = 0; i < count; ++i)
+            {
+                installedUpdates.Add(new InstalledUpdate(Title: history[i].Title, InstalledDate: history[i].Date));
+            }
+            return installedUpdates;
+        }
+
+        public Tuple<int, DateTime, List<IUpdate>> GetOutstandingUpdates(bool online = false)
+        {
+            IUpdateSearcher updateSearcher = GetNewSearcher(online: online);
+            ISearchResult requiredPatches = updateSearcher.Search("IsInstalled=0 And IsHidden=0");
+            int countRequired = requiredPatches.Updates.Count;
+            DateTime oldestUpdate = new DateTime();
+            List<IUpdate> updates = new List<IUpdate>();
+            foreach (IUpdate update in requiredPatches.Updates)
+            {
+                updates.Add(update);
+                if (update.LastDeploymentChangeTime > oldestUpdate)
+                {
+                    oldestUpdate = update.LastDeploymentChangeTime;
+                }
+            }
+            return new Tuple<int, DateTime, List<IUpdate>>(countRequired, oldestUpdate, updates);
+        }
+
+        public Tuple<DateTime?,DateTime?> GetDatesLastSearchAndInstall()
+        {
+            var auc = new AutomaticUpdates();
+
+            DateTime? lastInstallationSuccessDateUtc = null;
+            if (auc.Results.LastInstallationSuccessDate is DateTime)
+                lastInstallationSuccessDateUtc = new DateTime(((DateTime)auc.Results.LastInstallationSuccessDate).Ticks, DateTimeKind.Utc);
+
+            DateTime? lastSearchSuccessDateUtc = null;
+            if (auc.Results.LastSearchSuccessDate is DateTime)
+                lastSearchSuccessDateUtc = new DateTime(((DateTime)auc.Results.LastSearchSuccessDate).Ticks, DateTimeKind.Utc);
+
+            return new Tuple<DateTime?, DateTime?>(lastSearchSuccessDateUtc, lastInstallationSuccessDateUtc);
+        }
+    }
+}
